7648699048608b4dd58f8138ece5eebd61fbcae4
Najrul Islam <mdnajrulislam.4738@gmail.com>
2025-07-01 05:09:47 +0600

    setup project and install @redux/toolkit and react-redux

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+
+function App() {
+
+  return (
+    <div>
+      <h1>Counter Application with Redux</h1>
+      <button className="increment">Increment</button>
+      <div className="count">0</div>
+      <button className="decrement">Decrement</button>
+    </div>
+  )
+}
+
+export default App
