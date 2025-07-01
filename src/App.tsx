63a2666d808c1d5c3bb068ea7b89d76f1c7d005d
Najrul Islam <mdnajrulislam.4738@gmail.com>
2025-07-01 06:14:25 +0600

    create counterSlice and hooks , set method dispatch, selector

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,33 @@
-
-
+import { decrement, increment } from "./redux/features/counter/counterSlice";
+import { useAppDispatch, useAppSelector } from "./redux/hooks";
 function App() {
+  const dispatch = useAppDispatch();
+  // const counter = useSelector((state) => state.counter);
+  const { count } = useAppSelector((state) => state.counter);
+
+  const handleIncrement = (amount: number) => {
+    dispatch(increment(amount));
+  };
+
+  const handleDecrement = () => {
+    dispatch(decrement());
+  };
 
   return (
     <div>
       <h1>Counter Application with Redux</h1>
-      <button className="increment">Increment</button>
-      <div className="count">0</div>
-      <button className="decrement">Decrement</button>
+      <button onClick={() => handleIncrement(5)} className="increment">
+        Increment by 5
+      </button>
+      <button onClick={() => handleIncrement(1)} className="increment">
+        Increment
+      </button>
+      <div className="count">{count}</div>
+      <button onClick={handleDecrement} className="decrement">
+        Decrement
+      </button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
